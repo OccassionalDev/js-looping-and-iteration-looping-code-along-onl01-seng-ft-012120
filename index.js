b6caa6039ca5f2f3aabb22613c6c89fdf48d2770
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-24 05:29:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+  let cardArray = [];
+  
+  for (let i = 0; i < array.length(); i++) {
+    cardArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return cardArray;
+}
