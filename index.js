fa4a11692e8ff70c694650fbeaa5ed65f9e20a45
Erica Schimmel <ericaschimmel01@gmail.com>
2020-04-24 05:29:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function writeCards(array, event) {
   let cardArray = [];
   
-  for (let i = 0; i < array.length(); i++) {
+  for (let i = 0; i < array.length; i++) {
     cardArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
   }
   return cardArray;
